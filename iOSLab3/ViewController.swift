b056b08e1594d4601d8dc3ef0aec8aaa56ba8737
Pawel <pawel.losek@outlook.com>
2016-11-06 20:06:03 +0100

    ready app

--- iOSLab3/ViewController.swift
+++ iOSLab3/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var clearAction: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        stopButton.enabled = false
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            //locationManager.startUpdatingLocation()
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Button actions
+    
+    @IBAction func startAction(sender: AnyObject) {
+        locationManager.startUpdatingLocation()
+        startButton.enabled = false
+        stopButton.enabled = true
+        
     }
+    
+    @IBAction func stopAction(sender: AnyObject) {
+        locationManager.stopUpdatingLocation()
+        startButton.enabled = true
+        stopButton.enabled = false
+    }
+    
+    
+    @IBAction func clearAction(sender: AnyObject) {
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    
+    //Utils
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        
+        
+        let myAnnotation: MKPointAnnotation = MKPointAnnotation()
+        myAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
+        mapView.addAnnotation(myAnnotation)
+        
+        var spanDelta = 0.0
+        if let speed = locations.last?.speed where speed > 0 {
+            spanDelta = speed/5000
+        }
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: spanDelta, longitudeDelta: spanDelta))
 
-
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
 }
 
